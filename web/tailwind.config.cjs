b81b124545c5395e886e043e7a8dd30d92e4cbfa
raffaez <rafaeleasouzas@gmail.com>
2023-02-17 14:04:21 -0300

    chore: setup classes

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      backgroundImage: {
+        "top-ilustra": "url('/src/assets/ilustra-01.png')",
+        "bottom-ilustra": "url('/src/assets/ilustra-02.png')",
+      },
+      colors: {
+        red: {
+          500: "#E45858",
+        },
+        purple: {
+          500: "#6246EA",
+        },
+        black: {
+          500: "#121214",
+        },
+        gray: {
+          500: "#7C7C8A",
+        },
+      },
+    },
   },
   plugins: [],
 };
