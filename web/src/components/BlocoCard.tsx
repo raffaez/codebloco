167d73e6c2f1325d14b58b8f13525b594d8205ba
raffaez <rafaeleasouzas@gmail.com>
2023-02-19 18:38:35 -0300

    feat: add share links

--- web/src/components/BlocoCard.tsx
+++ web/src/components/BlocoCard.tsx
@@ -1,41 +1,89 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Bloco } from "../models/Bloco";
-import { CalendarIcon, MapPinIcon } from "@heroicons/react/20/solid";
+import { CalendarIcon, MapPinIcon, ShareIcon } from "@heroicons/react/20/solid";
 import { urlFor } from "../lib/sanity";
+import { Popover, Transition } from "@headlessui/react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {
   bloco: Bloco;
 };
 
 function BlocoCard({ bloco }: Props) {
+  const msg = `Oi! Eu vou no bloco ${bloco.nome} no dia ${bloco.data} às ${bloco.horario}. Vem comigo!`;
+  const wppUrl = `https://wa.me/?text=${encodeURIComponent(msg)}`;
+  const ttUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    msg
+  )}`;
+  const fbUrl = `https://www.facebook.com/sharer/sharer.php?u=https://www.codebloco.vercel.app&quote=${encodeURIComponent(
+    msg
+  )}`;
+
   return (
-    <div className="w-96 h-80 rounded-lg flex flex-col border border-gray-200">
-      <div className="h-40 bg-purple-500 rounded-t-lg">
-        <img
-          src={urlFor(bloco.imagem).url()}
-          className="h-40 w-full object-cover object-center rounded-t-lg opacity-80"
-          alt=""
-        />
-      </div>
-      <div className="px-5 h-full flex flex-col justify-center space-y-2">
-        <h3 className="text-lg font-bold text-black-500">{bloco.nome}</h3>
-        <p className="text-md text-black-500/75">{bloco.descricao}</p>
-        <div className="flex items-center justify-between">
-          <div className="flex flex-row space-x-1.5">
-            <MapPinIcon className="h-5 w-5 text-red-500" />
-            <span className="text-base text-black-500/75">
-              {bloco.cidade} - {bloco.estado}
-            </span>
+    <Popover className="relative">
+      <div className="w-96 h-80 rounded-lg flex flex-col border border-gray-200">
+        <div className="h-40 bg-purple-500 rounded-t-lg">
+          <img
+            src={urlFor(bloco.imagem).url()}
+            className="h-40 w-full object-cover object-center rounded-t-lg opacity-80"
+            alt=""
+          />
+        </div>
+        <div className="px-5 h-full flex flex-col justify-center space-y-2">
+          <div className="flex justify-between">
+            <h3 className="text-lg font-bold text-black-500">{bloco.nome}</h3>
+            <Popover.Button>
+              <ShareIcon className="w-6 h-6" />
+            </Popover.Button>
           </div>
-          <div className="flex flex-row space-x-1.5">
-            <CalendarIcon className="h-5 w-5 text-red-500" />
-            <span className="text-base text-black-500/75">
-              {bloco.data} às {bloco.horario}
-            </span>
+          <p className="text-md text-black-500/75">{bloco.descricao}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-row space-x-1.5">
+              <MapPinIcon className="h-5 w-5 text-red-500" />
+              <span className="text-base text-black-500/75">
+                {bloco.cidade} - {bloco.estado}
+              </span>
+            </div>
+            <div className="flex flex-row space-x-1.5">
+              <CalendarIcon className="h-5 w-5 text-red-500" />
+              <span className="text-base text-black-500/75">
+                {bloco.data} às {bloco.horario}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-200"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Popover.Panel className="absolute bottom-[105px] right-5 z-50">
+          <div className="h-10 w-32 bg-white rounded-md flex flex-row items-center justify-end space-x-2">
+            <SocialIcon
+              url={wppUrl}
+              network="whatsapp"
+              style={{ height: 28, width: 28 }}
+              target="_blank"
+            />
+            <SocialIcon
+              url={ttUrl}
+              style={{ height: 28, width: 28 }}
+              target="_blank"
+            />
+            <SocialIcon
+              url={fbUrl}
+              style={{ height: 28, width: 28 }}
+              target="_blank"
+            />
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 }
 
