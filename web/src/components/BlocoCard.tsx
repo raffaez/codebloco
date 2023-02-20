bed5ff3b9fffcba4e610629982f748ba0f9fe7b1
raffaez <rafaeleasouzas@gmail.com>
2023-02-20 03:34:06 -0300

    style: make social popover transition right to left

--- web/src/components/BlocoCard.tsx
+++ web/src/components/BlocoCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 };
 
 function BlocoCard({ bloco }: Props) {
-  const msg = `Oi! Eu vou no bloco ${bloco.nome} no dia ${bloco.data} às ${bloco.horario}. Vem comigo!`;
+  const msg = `Oi! Eu vou no bloco ${bloco.nome} no dia ${bloco.data} às ${bloco.horario}. Vem comigo! #boraCodar #codeBloco`;
   const wppUrl = `https://wa.me/?text=${encodeURIComponent(msg)}`;
   const ttUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
     msg
@@ -55,14 +55,14 @@ function BlocoCard({ bloco }: Props) {
       </div>
       <Transition
         as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
+        enter="transition ease-in duration-200"
+        enterFrom="opacity-0 translate-x-1"
+        enterTo="opacity-100 translate-x-0"
         leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
+        leaveFrom="opacity-100 translate-x-0"
+        leaveTo="opacity-0 translate-x-1"
       >
-        <Popover.Panel className="absolute bottom-[105px] right-5 z-50">
+        <Popover.Panel className="absolute bottom-[105px] right-[15px] z-50">
           <div className="h-10 w-32 bg-white rounded-md flex flex-row items-center justify-end space-x-2">
             <SocialIcon
               url={wppUrl}
